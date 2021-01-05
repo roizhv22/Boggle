ac336c301c2c75cb098c98af03f6e11bf03610ac
roizhv22 <roi.zehavi@mail.huji.ac.il>
2021-01-05 19:17:05 +0200

    upload

---
+++ boggle_board_randomizer.py
@@ -0,0 +1,35 @@
+import random
+
+BOARD_SIZE = 4
+LETTERS = [
+    ['A', 'E', 'A', 'N', 'E', 'G'],
+    ['A', 'H', 'S', 'P', 'C', 'O'],
+    ['A', 'S', 'P', 'F', 'F', 'K'],
+    ['O', 'B', 'J', 'O', 'A', 'B'],
+    ['I', 'O', 'T', 'M', 'U', 'C'],
+    ['R', 'Y', 'V', 'D', 'E', 'L'],
+    ['L', 'R', 'E', 'I', 'X', 'D'],
+    ['E', 'I', 'U', 'N', 'E', 'S'],
+    ['W', 'N', 'G', 'E', 'E', 'H'],
+    ['L', 'N', 'H', 'N', 'R', 'Z'],
+    ['T', 'S', 'T', 'I', 'Y', 'D'],
+    ['O', 'W', 'T', 'O', 'A', 'T'],
+    ['E', 'R', 'T', 'T', 'Y', 'L'],
+    ['T', 'O', 'E', 'S', 'S', 'I'],
+    ['T', 'E', 'R', 'W', 'H', 'V'],
+    ['N', 'U', 'I', 'H', 'M', 'QU']
+]
+
+
+def randomize_board(dice_list=LETTERS):
+    dice_indices = list(range(len(dice_list)))
+    random.shuffle(dice_indices)
+    board = []
+    for i in range(BOARD_SIZE):
+        row = []
+        for j in range(BOARD_SIZE):
+            die = dice_list[dice_indices[i * 4 + j]]
+            letter = random.choice(die)
+            row.append(letter)
+        board.append(row)
+    return board
---
+++ ex12_utils.py
@@ -0,0 +1,10 @@
+def load_words_dict(file_path):
+    pass
+
+
+def is_valid_path(board, path, words):
+    pass
+
+
+def find_length_n_words(n, board, words):
+    pass
